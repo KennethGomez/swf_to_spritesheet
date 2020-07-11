c0ad09dab765c014b80e63421c29e5e60b43ba2a
KennethGomez <kennethgomezfdz@gmail.com>
2020-07-11 20:18:05 +0200

    add: Implement sheep package sprite sheet packaging
    Signed-off-by: KennethGomez <kennethgomezfdz@gmail.com>

--- src/images/spritesheet.rs
+++ src/images/spritesheet.rs
@@ -2,8 +2,11 @@ use image::{ImageBuffer, RgbaImage};
 
 use crate::images::image::Image;
 
+extern crate sheep;
+
 pub struct SpriteSheet {
 	pub buffer: RgbaImage,
+	pub meta: Option<sheep::SerializedSpriteSheet>
 }
 
 impl SpriteSheet {}
@@ -12,10 +15,46 @@ pub fn create_spritesheet(images: Vec<Image>) -> Option<SpriteSheet> {
 	if images.len() == 0 {
 		None
 	} else {
+		let mut sprites = Vec::<sheep::InputSprite>::new();
+
+		for image in images {
+			let input_sprite = sheep::InputSprite {
+				dimensions: image.buffer.dimensions(),
+				bytes: image.buffer.into_raw(),
+			};
+
+			sprites.push(input_sprite)
+		}
+
+		let results = sheep::pack::<sheep::SimplePacker>(sprites, 4, ());
+
+		let sprite_sheet = results
+			.into_iter()
+			.next()
+			.expect("Should have returned a spritesheet");
+
+		let meta = sheep::encode::<sheep::AmethystFormat>(&sprite_sheet, ());
+
+		let buffer = image::RgbaImage::from_vec(
+			sprite_sheet.dimensions.0,
+			sprite_sheet.dimensions.1,
+			sprite_sheet.bytes,
+		).expect("Failed to construct image from sprite sheet bytes");
+
+		Some(SpriteSheet {
+			buffer,
+			meta: Some(meta)
+		})
+	}
+}
+
+pub fn create_spritesheet_native(images: Vec<Image>) -> Option<SpriteSheet> {
+	if images.len() == 0 {
+		None
+	} else {
 		let width = images
 			.iter()
-			.max_by_key(|i| i.buffer.width())
-			.unwrap()
+			.max_by_key(|i| i.buffer.width())?
 			.buffer
 			.width();
 
@@ -23,7 +62,7 @@ pub fn create_spritesheet(images: Vec<Image>) -> Option<SpriteSheet> {
 		let mut data: Vec<u8> = Vec::<u8>::new();
 
 		for image in images.iter() {
-			height += image.buffer.height() + 10;
+			height += image.buffer.height();
 
 			let old_data = image.buffer.clone().into_raw();
 			let bf: image::RgbaImage = image::ImageBuffer::from_raw(
@@ -33,7 +72,7 @@ pub fn create_spritesheet(images: Vec<Image>) -> Option<SpriteSheet> {
 			)
 			.unwrap();
 			let mut new_img: image::RgbaImage =
-				image::ImageBuffer::new(width, image.buffer.height() + 10);
+				image::ImageBuffer::new(width, image.buffer.height());
 
 			for (x, y, rgba) in bf.enumerate_pixels() {
 				new_img.put_pixel(x, y, *rgba);
@@ -44,6 +83,7 @@ pub fn create_spritesheet(images: Vec<Image>) -> Option<SpriteSheet> {
 
 		Some(SpriteSheet {
 			buffer: ImageBuffer::from_vec(width, height, data).unwrap(),
+			meta: None
 		})
 	}
 }
